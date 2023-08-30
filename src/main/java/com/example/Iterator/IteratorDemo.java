cc06254ed5ad0166ee8867b879b3733f5c31d809
Kerim Şentürk <kerimsenturk2002@outlook.com>
2023-08-31 00:33:48 +0300

    Iterator interface explained.

---
+++ src/main/java/com/example/Iterator/IteratorDemo.java
@@ -0,0 +1,85 @@
+package com.example.Iterator;
+
+import java.util.*;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        /**
+         * Iterator interface'i üzerinde gezinebileceğimiz
+         * yani Iterable sınıflar üzerinde kullanılır.
+         */
+
+        /**
+         * Her collection bir iterator'a sahiptir
+         * Bu örnekte map sınıfını kullanalım.
+         */
+        Map<Integer, String> map = new HashMap<>(
+                Map.of(
+                        1, "One",
+                        2, "Two",
+                        3, "Three",
+                        10, "Ten"));
+
+        /**
+         * Map sınıfı EntrySet ve KeySet olarak iki farklı set'e sahip
+         * olduğu için her biri için ayrı iterator'u vardır. Bu örnekte
+         * EntrySet'i kullandık.
+         */
+        Iterator<Map.Entry<Integer, String>> entrySetIterator = map.entrySet().iterator();
+
+        /**
+         * hasNext() methodu collection'nın sonuna gelip gelmediğimizi
+         * gösteren boolean döndürür.
+         */
+
+        /**
+         * next() methodu geriye iterator'un üzerinde bulunduğu değeri
+         * döndürür ve kendi içerisindeki index değerini bir artırararak
+         * bir sonraki elemanın üzerine geçer.
+         */
+        while (entrySetIterator.hasNext()){
+            System.out.println("Entry set value : " + entrySetIterator.next());
+        }
+
+        /**
+         * Bir iterator bir kere yapının sonuna kadar gezdikten sonra geri dönemez.
+         * Bu yüzden tekrardan mevcut yapının iterator'unu almamız gerekir.
+         */
+        entrySetIterator = map.entrySet().iterator();
+
+        /**
+         * remove() methodu iterator'un üzerinde bulunduğu elemanı yapıdan siler.
+         * Bu methodu kullanmak için önce iterator.next() methodunun bir kere
+         * çağrılmış olması gerekir. Aksi halde IllegalStateException döndürür.
+         * remove() methodunu çağırırken sürekli next() yapmak unutulmamılıdır.
+         */
+        while(entrySetIterator.hasNext()){
+            entrySetIterator.next();
+            entrySetIterator.remove();
+        }
+
+        Map<String, String> countryCapitalMap = new HashMap<>(
+                Map.of(
+                        "TUR", "Ankara",
+                        "USA", "Washington",
+                        "CHI", "Pekin",
+                        "POL", "Varşova"));
+
+        Iterator<String> keyIterator = countryCapitalMap.keySet().iterator();
+
+        keyIterator.next();
+        keyIterator.next();
+
+        /**
+         * forEachRemaining() methodu iterator.next() yaparak
+         * parametre ile verilen Consumer methodu veriler
+         * üzerinde uygular.
+
+         * Bu işi yaparken iterator mevcutta neredeyse oradan başlar
+         * ve yapının sonuna gider.
+         * Bu yüzden önceden iki kere next() işlemini uyguladğımız
+         * için kalan veriler için println() fonksiyonunu uyguladı.
+         */
+        keyIterator.forEachRemaining(System.out::println);
+    }
+}
